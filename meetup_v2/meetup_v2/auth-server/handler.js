a90d039836527e098b27f4ffb57bf19204f23d77
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-01-28 11:02:32 -0500

    fix secret key in handler.js

--- meetup_v2/meetup_v2/auth-server/handler.js
+++ meetup_v2/meetup_v2/auth-server/handler.js
@@ -6,7 +6,7 @@ module.exports.getAccessToken = async (event) => {
 
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
     + '?client_id=3dssascu6mej4fuptij43jdbh4'
-    + '&client_secret=v1A/UF3x+u2vzYrj5AsIS5frj+Z9gmdPCgd6qSsy'
+    + '&client_secret=eubts7ml852ltfal2m5kiidta7'
     + '&grant_type=authorization_code'
     + '&redirect_uri=https://bxchang04.github.io/meetup_v2'
     + '&code=dbe12ed8f2d65e8f1a790cc01d519a54';
