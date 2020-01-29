d8cbc6c9485276ebb8909b2ded86310c32c92787
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-01-28 20:08:01 -0500

    encounter cors error on last step

--- meetup_v2/meetup_v2/auth-server/handler.js
+++ meetup_v2/meetup_v2/auth-server/handler.js
@@ -24,3 +24,25 @@ module.exports.getAccessToken = async (event) => {
     }),
   };
 };
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=3dssascu6mej4fuptij43jdbh4'
+    + '&client_secret=eubts7ml852ltfal2m5kiidta7'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.refresh_token;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
