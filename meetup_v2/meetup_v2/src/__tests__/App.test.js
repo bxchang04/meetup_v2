5ad25f6bea131e7eb7ed9b597a2fea2f0a8adf63
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-01-30 17:37:18 -0500

    complete integration tests

--- meetup_v2/meetup_v2/src/__tests__/App.test.js
+++ meetup_v2/meetup_v2/src/__tests__/App.test.js
@@ -43,13 +43,12 @@ describe('<App /> integration', () => {
     AppWrapper.instance().updateEvents(1.1, 1.2);
     await AppWrapper.update();
     expect(AppWrapper.state('events')).toEqual(mockEvents.events);
-    AppWrapper.unmount();
   });
 
   test('render correct list of events', () => {
     const AppWrapper = mount(<App />);
     AppWrapper.setState({ events: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] });
-    expect(AppWrapper.find('.Event')).toHaveLength(4);
+    expect(AppWrapper.find('.event')).toHaveLength(4);
     AppWrapper.unmount();
   });
 });
