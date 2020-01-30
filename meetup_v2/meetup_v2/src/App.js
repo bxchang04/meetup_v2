f1c1bae9f39c14f6fa6cef85a13d682e9513a9ec
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-01-30 10:12:32 -0500

    4.3 final submission

--- meetup_v2/meetup_v2/src/App.js
+++ meetup_v2/meetup_v2/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
+        <NumberOfEvents />
       </div>
     );
   }
