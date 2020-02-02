7e60032f514eb79b3ee6c0adb3f23d8e78198f1b
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-02 15:25:14 -0500

    4.4 debugging with Hugo

--- meetup_v2/meetup_v2/src/__tests__/Event.test.js
+++ meetup_v2/meetup_v2/src/__tests__/Event.test.js
@@ -9,7 +9,55 @@ describe('<Event /> component', () => {
 
   let EventWrapper;
   beforeAll(() => {
-    EventWrapper = shallow(<Event />);
+    EventWrapper = shallow(<Event event={{
+      created: 1536239636000,
+        duration: 7200000,
+        id: 'rvwdfqybcdbcb',
+        name: 'Saturday 9-11 am Tennis at Newton South High School-Intermediate skill level &up',
+        rsvp_limit: 16,
+        date_in_series_pattern: false,
+        status: 'upcoming',
+        time: 1580565600000,
+        local_date: '2020-02-01',
+        local_time: '09:00',
+        updated: 1536239636000,
+        utc_offset: -18000000,
+        waitlist_count: 0,
+        yes_rsvp_count: 1,
+        venue: {
+        id: 1082097,
+        name: 'Newton South High School',
+        lat: 42.31231689453125,
+        lon: -71.18464660644531,
+        repinned: true,
+        address_1: '180 Brandeis Rd. ',
+        city: 'Newton',
+        country: 'us',
+        localized_country_name: 'USA',
+        zip: '02459',
+        state: 'MA'
+        },
+        group: {
+        created: 1269228542000,
+        name: 'Boston Tennis Players',
+        id: 1618259,
+        join_mode: 'open',
+        lat: 42.369998931884766,
+        lon: -71.08999633789062,
+        urlname: 'boston-tennis',
+        who: 'Boston Tennis Players',
+        localized_location: 'Cambridge, MA',
+        state: 'MA',
+        country: 'us',
+        region: 'en_US',
+        timezone: 'US/Eastern'
+        },
+        link: 'https://www.meetup.com/boston-tennis/events/rvwdfqybcdbcb/',
+        description: '<p>This Meetup is for players with intermediate skills and up.</p> <p>Weekly Saturday morning tennis at Newton South High School. Look for the regulars and say hello to people.</p> <p>The nets at Newton South have been put back up.</p> <p>I am continuing this meetup because of high interest in the Brookline meetup. People who live near Newton are welcome to come. Hope to see you guys there!</p> ',
+        "how_to_find_us": "Ask anybody playing if they are there for the meetup. Look for regulars.",
+        visibility: 'public',
+        member_pay_fee: false
+    }}/>);
   });
 
   test('test that componet is rendered', () => {
@@ -47,63 +95,10 @@ describe('<Event /> component', () => {
     EventWrapper.setState({
       showDetails: false
     });
-    EventWrapper.find('.event__Overview button').simulate('click');
+  // test('click on button should show details', () => {
+  //   EventWrapper = shallow(<Event prop={{showDetails: false}}/>)
+  //   EventWrapper.find('.event__Overview button').simulate('click');
     expect(EventWrapper.state('showDetails')).toBe(true);
+    // expect(EventWrapper.props('showDetails')).toBe(true);
   });
-
-  test('set mock eventdata as state', () => {
-      EventWrapper.setState({
-        event: { //need to remove quotes?
-          "created": 1536239636000,
-          "duration": 7200000,
-          "id": "rvwdfqybcdbcb",
-          "name": "Saturday 9-11 am Tennis at Newton South High School-Intermediate skill level &up",
-          "rsvp_limit": 16,
-          "date_in_series_pattern": false,
-          "status": "upcoming",
-          "time": 1580565600000,
-          "local_date": "2020-02-01",
-          "local_time": "09:00",
-          "updated": 1536239636000,
-          "utc_offset": -18000000,
-          "waitlist_count": 0,
-          "yes_rsvp_count": 1,
-          "venue": {
-          "id": 1082097,
-          "name": "Newton South High School",
-          "lat": 42.31231689453125,
-          "lon": -71.18464660644531,
-          "repinned": true,
-          "address_1": "180 Brandeis Rd. ",
-          "city": "Newton",
-          "country": "us",
-          "localized_country_name": "USA",
-          "zip": "02459",
-          "state": "MA"
-          },
-          "group": {
-          "created": 1269228542000,
-          "name": "Boston Tennis Players",
-          "id": 1618259,
-          "join_mode": "open",
-          "lat": 42.369998931884766,
-          "lon": -71.08999633789062,
-          "urlname": "boston-tennis",
-          "who": "Boston Tennis Players",
-          "localized_location": "Cambridge, MA",
-          "state": "MA",
-          "country": "us",
-          "region": "en_US",
-          "timezone": "US/Eastern"
-          },
-          "link": "https://www.meetup.com/boston-tennis/events/rvwdfqybcdbcb/",
-          "description": "<p>This Meetup is for players with intermediate skills and up.</p> <p>Weekly Saturday morning tennis at Newton South High School. Look for the regulars and say hello to people.</p> <p>The nets at Newton South have been put back up.</p> <p>I am continuing this meetup because of high interest in the Brookline meetup. People who live near Newton are welcome to come. Hope to see you guys there!</p> ",
-          "how_to_find_us": "Ask anybody playing if they are there for the meetup. Look for regulars.",
-          "visibility": "public",
-          "member_pay_fee": false
-        },
-      });
-      console.log(EventWrapper.state('event'));
-      expect(EventWrapper.state('event').name).toBe('Saturday 9-11 am Tennis at Newton South High School-Intermediate skill level &up');
-    });
-  });
+});
