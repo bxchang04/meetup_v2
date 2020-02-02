67b1e45a1079c3f96c272c159841667308b10986
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-01 20:18:06 -0500

    4.4 completed BDD

--- meetup_v2/meetup_v2/src/api.js
+++ meetup_v2/meetup_v2/src/api.js
@@ -1,31 +1,109 @@
 import { mockEvents } from './mock-events';
+import axios from 'axios';
 
 async function getSuggestions(query) {
-  return [
-    {
-      city: 'Munich',
-      country: 'de',
-      localized_country_name: 'Germany',
-      name_string: 'Munich, Germany',
-      zip: 'meetup3',
-      lat: 48.14,
-      lon: 11.58
-    },
-    {
-      city: 'Munich',
-      country: 'us',
-      localized_country_name: 'USA',
-      state: 'ND',
-      name_string: 'Munich, North Dakota, USA',
-      zip: '58352',
-      lat: 48.66,
-      lon: -98.85
-    }
-  ];
+  if (window.location.href.startsWith('http://localhost')) {
+    return [
+      {
+        city: 'Munich',
+        country: 'de',
+        localized_country_name: 'Germany',
+        name_string: 'Munich, Germany',
+        zip: 'meetup3',
+        lat: 48.14,
+        lon: 11.58
+      },
+      {
+        city: 'Munich',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'ND',
+        name_string: 'Munich, North Dakota, USA',
+        zip: '58352',
+        lat: 48.66,
+        lon: -98.85
+      }
+    ];
+  }
+
+  const token = await getAccessToken();
+  if (token) {
+    const url = 'https://api.meetup.com/find/locations?&sign=true&photo-host=public&query='
+      + query
+      + '&access_token=' + token;
+    const result = await axios.get(url);
+    return result.data;
+  }
+  return [];
 }
 
 async function getEvents(lat, lon) {
-  return mockEvents.events;
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockEvents.events;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    let url = 'https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public'
+      + '&access_token=' + token;
+    // lat, lon is optional; if you have a lat and lon, you can add them
+    if (lat && lon) {
+      url += '&lat=' + lat + '&lon=' + lon;
+    }
+    const result = await axios.get(url);
+    return result.data.events;
+  }
+}
+
+async function getAccessToken() {
+  const accessToken = localStorage.getItem('access_token');
+
+  if (!accessToken) {
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+
+    if (!code) {
+      window.location.href = 'https://secure.meetup.com/oauth2/authorize?client_id=3dssascu6mej4fuptij43jdbh4&response_type=code&redirect_uri=https://bxchang04.github.io/meetup_v2';
+      return null;
+    }
+
+    return getOrRenewAccessToken('get', code);
+  }
+
+  const lastSavedTime = localStorage.getItem('last_saved_time');
+
+  if (accessToken && (Date.now() - lastSavedTime < 3600000)) {
+    return accessToken;
+  }
+
+  const refreshToken = localStorage.getItem('refresh_token');
+
+  return getOrRenewAccessToken('renew', refreshToken);
+}
+
+async function getOrRenewAccessToken(type, key) {
+  let url;
+  if (type === 'get') {
+    // Lambda endpoint to get token by code
+    url = 'https://c5usrytrsg.execute-api.eu-central-1.amazonaws.com/dev/api/token/'
+      + key;
+  } else if (type === 'renew') {
+    // Lambda endpoint to get token by refresh_token
+    url = 'https://c5usrytrsg.execute-api.eu-central-1.amazonaws.com/dev/api/refresh/'
+      + key;
+  }
+
+  // Use Axios to make a GET request to the endpoint
+  const tokenInfo = await axios.get(url);
+
+  // Save tokens to localStorage together with a timestamp
+  localStorage.setItem('access_token', tokenInfo.data.access_token);
+  localStorage.setItem('refresh_token', tokenInfo.data.refresh_token);
+  localStorage.setItem('last_saved_time', Date.now());
+
+  // Return the access_token
+  return tokenInfo.data.access_token;
 }
 
 export { getSuggestions, getEvents };
