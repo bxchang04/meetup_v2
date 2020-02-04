575ef25d583500e5b88d396d7acfe9a731a796dc
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-04 13:28:25 -0500

    4.6 atatus error does not receive first event

--- meetup_v2/meetup_v2/src/index.js
+++ meetup_v2/meetup_v2/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('265dd06585c64899a04b4e5272208d4f').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
