617fb5630587c1cee3d0701120e372d65ea287e0
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-04 11:22:38 -0500

    4.5 add tests and puppeteer

--- meetup_v2/meetup_v2/src/__tests__/EndToEnd.test.js
+++ meetup_v2/meetup_v2/src/__tests__/EndToEnd.test.js
@@ -1,16 +1,28 @@
 import puppeteer from 'puppeteer';
 
-
 describe('show/hide an event details', () => {
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.Event');
+  });
 
-    await page.waitForSelector('.event'); //lowercase E
-
-    const extra = await page.$('.event .extra'); //lowercase E
-    expect(extra).toBeNull();
+  afterAll(() => {
     browser.close();
+  });
+
+  test('An event element is collapsed by default', async () => {
+    const extra = await page.$('.Event .extra');
+    expect(extra).toBeNull();
+  });
+
+  test('User can expand an event to see its details', async () => {
+    await page.click('.Event .details-btn');
+
+    const extra = await page.$('.Event .extra');
+    expect(extra).toBeDefined();
   });
 });
