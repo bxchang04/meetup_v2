d79e1e8d602350b0d32f3bfb363b12e9086b4eda
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-04 23:31:45 -0500

    4.9 initial commit

--- meetup_v2/meetup_v2/src/Event.js
+++ meetup_v2/meetup_v2/src/Event.js
@@ -1,7 +1,7 @@
-//need to fix HTML section
+//study https://github.com/envincebal/meetup/blob/master/src/Event.js
 
 import React, { Component } from 'react';
-import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer, Cell} from 'recharts';
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer, Cell, Line} from 'recharts';
 
 class Event extends Component {
   state = {
@@ -10,73 +10,70 @@ class Event extends Component {
   }
 
   handleShowDetails = () => {
-    this.setState({ showDetails: !this.state.showDetails });
-    const rsvpCount = this.props.event.yes_rsvp_count;
-    const rsvpMax = this.props.event.rsvp_limit;
-    if (rsvpCount == rsvpMax)  {
-      this.setState({ infoText: `This event is completely booked.` })
-    } else {
-      this.setState({ infoText: '' });
+    if(this.state.showDetails === false) {
+      this.setState({ showDetails: true });
+    }
+    else {
+      this.setState({ showDetails: false });
     }
   }
 
-  getData() {
-    let reservations = this.props.event.yes_rsvp_count;
-    let vacancies = this.props.event.rsvp_limit - this.props.event.yes_rsvp_count;
+  getEventData = () => {
+    const {event} = this.props;
+    const spotsTaken = event.yes_rsvp_count;
+    const spotsFree = event.rsvp_limit - spotsTaken;
 
     return (
       [
-        {name: 'reservations', value: reservations },
-        {name: 'vacancies', value: vacancies }
+        {name: "Attending", value: spotsTaken},
+        {name: "Spots Open", value: spotsFree}
       ]
-    );
+    )
   }
 
   render() {
-    const showDetails = this.state.showDetails;
-
-    return(
+    const {event} = this.props;
+    let colors = ["#e34542", "#43e06d"]
+    return (
       <div className="event">
-        <div className="event__Overview">
-          <p className="event__Overview--localDate">{this.props.event.local_date}</p>
-          <p className="event__Overview--name">{this.props.event.name}</p>
-          <p className="group_name">GROUP: {event.group.name}</p>
-          <p className="event_rsvp">{event.yes_rsvp_count} people signed up for this meeting</p>
-          {/*<button className="details-btn" onClick={() => this.handleShowDetails()}>show details</button>*/}
-          <div className="eventDetails">
-          { event.rsvp_limit &&
-          <ResponsiveContainer width="99%" height="99%">
-            <PieChart width={300} height={200}>
-            <Legend />
-              <Pie data={this.getData()} fill="#8884d8" label>
-              {
-                this.getData().map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={colors[index]}/>
-                ))
-              }
-              </Pie>
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-          }
-          <WarningAlert text={this.state.infoText} /><br/>
-          { event.venue &&
-            <p className="address">
-              <span>{event.venue.name},</span>
-              <span>{event.venue.address_1},</span>
-              <span>{event.venue.address_2},</span>
-              <span>{event.venue.address_3},</span>
-              <span>{event.venue.city},</span>
-              <span>{event.venue.localized_country_name}</span>
-            </p>
-          }
-            <div>
-              <div className="description" dangerouslySetInnerHTML={{ __html: event.description }} />
-              <p className="visibility">{event.visibility}</p>
-              <a className="link" href={event.link}>Event Link</a>
-            </div>
-          </div>
+        <p>{event.local_time + " - " + event.local_date}</p>
+        <p className="event-name">{event.name}</p>
+        <p><span className="label">Group:</span> {event.group && event.group.name}</p>
+        <p className="event-count"><span className="label">RSVP: </span>{event.yes_rsvp_count} people are going</p>
+        {event.venue &&
+          <p className="event-address"><span className="label">Address: </span>{ event.venue.address_1}, {event.venue.city}, {event.group.state ? event.group.state : event.venue.localized_country_name}</p>
+        }
+        <p className="event-venue"><span className="label">Venue: </span>{event.venue && event.venue.name}</p>
+        {this.state.showDetails && (
+          <div className="event-details">
+            <hr/>
+            {this.props.event.rsvp_limit ? (
+              <ResponsiveContainer height={250}>
+              <PieChart width={200} height={200}>
+                <Pie isAnimationActive={false} data={this.getEventData()} dataKey="value" cx="50%" cy="50%" outerRadius={80} label>
+                  {
+                    this.getEventData().map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={colors[index]}/>
+                    ))
+                  }
+                </Pie>
+                <Tooltip />
+                <Legend verticalAlign="top" height={30}>
+                  <Line name="Attending" type="monotone" dataKey="spotsTaken" stroke="#8884d8" />
+                  <Line name="Spots Open" type="monotone" dataKey="spotsFree" stroke="#82ca9d" />
+                </Legend>
+              </PieChart>
+            </ResponsiveContainer>
+            ):(
+              ""
+            )
+            }
 
+            <p className="event-description" dangerouslySetInnerHTML={{__html: event.description}}></p>
+            <a className="event-link" href={event.link}>Event Page</a>
+          </div>
+        )}
+        <button onClick={this.handleShowDetails} className="details-btn">Details</button>
       </div>
     );
   }
