3bbb7e58a3199f71af14997e7163ebb11875d6a5
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-08 17:00:47 -0500

    Updates

--- precache-manifest.62a3b64528832ed168d4fc91aa38c564.js
+++ precache-manifest.62a3b64528832ed168d4fc91aa38c564.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab048b225f24a0825bcbba109dc6293a",
+    "revision": "87523d17fd61c71924f97bced3eeccea",
     "url": "/meetup_v2/index.html"
   },
   {
-    "revision": "f0b883b37393cbb03753",
+    "revision": "7ac73eff9d95cfbc2b80",
     "url": "/meetup_v2/static/css/main.72eed2df.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup_v2/static/js/2.fb8a4da6.chunk.js.LICENSE"
   },
   {
-    "revision": "f0b883b37393cbb03753",
-    "url": "/meetup_v2/static/js/main.41d4c89e.chunk.js"
+    "revision": "7ac73eff9d95cfbc2b80",
+    "url": "/meetup_v2/static/js/main.5aac05d9.chunk.js"
   },
   {
     "revision": "813e8b45b2f8bbf41eac",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup_v2/precache-manifest.d97a609acb83fe87a00d577ea178c87b.js"
+  "/meetup_v2/precache-manifest.62a3b64528832ed168d4fc91aa38c564.js"
 );
 
 self.addEventListener('message', (event) => {
